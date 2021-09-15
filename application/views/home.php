d584df981b6a6242b63d195f6b89f2ccd605a8f1
Dana Sugu <dana.sugu@gmail.com>
2021-09-15 20:00:35 +0300

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -13,5 +13,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </head>
 <body>
   Hi
+
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 </body>
 </html>
