926d7ff5d50fc40ee990ddb502790750b7dd1fea
Dana Sugu <dana.sugu@gmail.com>
2021-09-15 20:02:57 +0300

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -12,7 +12,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <title>Shop</title>
 </head>
 <body>
-  Hi
+ <div class="container">
+   <div class="row">
+     <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+   </div>
+ </div>
 
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
