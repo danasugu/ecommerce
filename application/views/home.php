e3e8e6ef69a0fd84356ded64c6c1605a9c4dcf61
Dana Sugu <dana.sugu@gmail.com>
2021-09-15 19:53:42 +0300

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -7,8 +7,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="<?=base_url()?>assets/css/bootstrap.min.css">
+<script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
   <title>Shop</title>
 </head>
 <body>
