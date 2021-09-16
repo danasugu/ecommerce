dd5917ec0cf77a521f9625e8f3bf787f1ba178d5
Dana Sugu <dana.sugu@gmail.com>
2021-09-16 10:18:53 +0300

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -20,18 +20,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
      <form>
   <div class="form-group">
     <label for="title">Title</label>
-    <input type="text" class="form-control" id="title">
+    <input type="text" class="form-control" id="title" name="title">
   </div>
   <div class="form-group">
     <label for="description">Description</label>
-    <textarea class="form-control" id="description"></textarea>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <textarea class="form-control" id="description" name="description"></textarea>
   </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Add new</button>
 </form>
    </div>
 </dic>
