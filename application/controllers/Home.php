1f0b0d3bcb98ed4c570f16adeca959e8c38a51a8
Dana Sugu <dana.sugu@gmail.com>
2021-09-18 20:19:40 +0300

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -11,15 +11,12 @@ class Home extends CI_Controller {
 	public function add_item() {
 		  $this->load->helper('form');
 			$this->load->library('form_validation');
-			
-                if ($this->form_validation->run() == FALSE)
+			$this->form_validation->set_rules('title', 'Title', 'required|min_length[3]|max_length[30]');
+                if ($this->form_validation->run())
                 {
-                        $this->load->view('myform');
+                     
                 }
-                else
-                {
-                        $this->load->view('formsuccess');
-                }
+
 			$this->load->view('add_item');
 	}
 }
