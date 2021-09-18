b5fc14bfffa8060f463783b8b3ff39278041b18d
Dana Sugu <dana.sugu@gmail.com>
2021-09-18 20:13:04 +0300

    Update add_item.php

--- application/views/add_item.php
+++ application/views/add_item.php
@@ -20,11 +20,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <?=form_open_multipart()?>
   <div class="form-group">
     <label for="title">Title</label>
-    <input type="text" class="form-control" id="title" name="title">
+    <input type="text" class="form-control" id="title" name="title" value="<?=set_value('title')?>">
   </div>
    <div class="form-group">
     <label for="price">Price</label>
-    <input type="number" class="form-control" min="0" step="0.01" id="price" name="price">
+    <input type="number" class="form-control" min="0" step="0.01" id="price" name="price" value="<?=set_value('price')?>">
   </div>
   <div class="form-group">
     <label for="image">Image</label>
@@ -32,7 +32,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   </div>
   <div class="form-group">
     <label for="description">Description</label>
-    <textarea class="form-control" id="description" name="description"></textarea>
+    <textarea class="form-control" id="description" name="description" value="<?=set_value('description')?>"></textarea>
   </div>
   <button type="submit" class="btn btn-success">Add new</button>
 <?=form_close()?>
